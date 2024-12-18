4072f0395ee1a8f28dce19ba934304199343df8d
Saumya Singh <singhsaumyait034@gmail.com>
2024-12-18 17:18:57 +0530

    Functions

---
+++ Day1/functionsType.ts
@@ -0,0 +1,16 @@
+function addition(a,b){
+    return a+b;
+}
+let lname = "Saumya";
+
+
+function stringData(data){
+    return data;
+}
+
+function addition1(a:number,b:number){
+    return a+b;
+}
+
+console.log(addition(5,6));
+console.log(stringData(lname));
