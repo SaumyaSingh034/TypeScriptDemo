1970a3aad603e9e64667e426fefe96e9abea47cd
Saumya Singh <singhsaumyait034@gmail.com>
2025-01-14 23:03:52 +0530

    Switch with Typescript

---
+++ Day4/switch.js
@@ -0,0 +1,27 @@
+var day = "wednesday";
+switch (day) {
+    case "sunday":
+        console.log("Hello Sunday");
+        break;
+    case "monday":
+        console.log("Hello Monday");
+        break;
+    case "tuesday":
+        console.log("Hello Tuesday");
+        break;
+    case "wednesday":
+        console.log("Hello Wednesday");
+        break;
+    case "thursday":
+        console.log("Hello Thursday");
+        break;
+    case "friday":
+        console.log("Hello Friday");
+        break;
+    case "saturday":
+        console.log("Hello Saturday");
+        break;
+    default:
+        console.log("Wrond Day");
+        break;
+}
